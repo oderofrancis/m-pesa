f8473be7707ece454fb4b440170942b0fe7b2c9a
oderofrancis <francisodero10@gmail.com>
2022-01-02 21:32:31 +0300

    second commit

--- mpesa_app/settings.py
+++ mpesa_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
